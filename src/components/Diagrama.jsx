87724d83ed521ff2f1a7f110c5fda5c2b9d5e540
onechvoglod <o.nechvoglod@gmail.com>
2020-07-17 10:02:35 +0300

    created link template

--- src/components/Diagrama.jsx
+++ src/components/Diagrama.jsx
@@ -2,12 +2,84 @@ import React from "react";
 import * as go from "gojs";
 import { ReactDiagram } from "gojs-react";
 import classes from "./Diagrama.module.scss";
+import { useContext } from "react";
+import { RedmineContext } from "../context/redmine/redmineContext";
+
+const ingURL =
+  "https://lh3.googleusercontent.com/proxy/FO0H1tQvc1NH-aWJj5MWVDqyLjqvopcbLVyte2rVhpCG5jZHgQagFyn2DTwdnkI5voI0Aox7BZwrY5qcpaCM4G3JIu37q7-ZZRLQOI8ieHp74VM-t6ai_HVQTb8";
 
 const nodeDataArra = [
-  { key: 0, text: "Алексей Безпаленко", color: "red", loc: "0 0" },
-  { key: 1, text: "Александр Чиняков", color: "orange", loc: "150 0" },
-  { key: 2, text: "Alexander Perel", color: "lightgreen", loc: "0 150" },
-  { key: 3, text: "Флександр присяжный", color: "pink", loc: "150 150" },
+  {
+    key: 0,
+    text: "Алексей Безпаленко",
+    color: "orange",
+    // loc: "0 0",
+    source: ingURL,
+  },
+  {
+    key: 1,
+    text: "Александр Чиняков",
+    color: "orange",
+    // loc: "150 0",
+    source: ingURL,
+    parent: 0,
+  },
+  {
+    key: 2,
+    text: "Alexander Perel",
+    color: "lightgreen",
+    // loc: "0 150",
+    source: ingURL,
+    parent: 0,
+  },
+  {
+    key: 3,
+    text: "Анастасия Беседина",
+    color: "pink",
+    // loc: "150 150",
+    source: ingURL,
+    parent: 1,
+  },
+  {
+    key: 3,
+    text: "Алла Мец",
+    color: "pink",
+    // loc: "150 150",
+    source: ingURL,
+    parent: 1,
+  },
+  {
+    key: 3,
+    text: "Александр присяжный",
+    color: "pink",
+    // loc: "150 150",
+    source: ingURL,
+    parent: 2,
+  },
+  {
+    key: 3,
+    text: "Алексей Битюков",
+    color: "pink",
+    // loc: "150 150",
+    source: ingURL,
+    parent: 2,
+  },
+  {
+    key: 3,
+    text: "Алексей Гладковский",
+    color: "pink",
+    // loc: "150 150",
+    source: ingURL,
+    parent: 3,
+  },
+  {
+    key: 3,
+    text: "Алина Петровская",
+    color: "pink",
+    // loc: "150 150",
+    source: ingURL,
+    parent: 3,
+  },
 ];
 
 function initDiagram() {
@@ -55,11 +127,20 @@ function initDiagram() {
       alternateNodeSpacing: 20, // расстояние между узлами внутри слоя - между братьями и сестрами.
     }),
 
-    model: $(go.GraphLinksModel, {
-      linkKeyProperty: "key", // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel ??
-    }),
+    // model: $(go.GraphLinksModel, {
+    //   linkKeyProperty: "key", // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel ??
+    // }),
   });
+  var model = $(go.TreeModel);
+  model.nodeDataArray = nodeDataArra;
+  myDiagram.model = model;
 
+  myDiagram.linkTemplate = $(
+    go.Link,
+    go.Link.Orthogonal,
+    { corner: 5, relinkableFrom: true, relinkableTo: true },
+    $(go.Shape, { strokeWidth: 2, stroke: "black" })
+  );
   //   var levelColors = [
   //     "#AC193D",
   //     "#2672EC",
@@ -117,10 +198,11 @@ function initDiagram() {
   myDiagram.nodeTemplate = $(
     go.Node,
     "Auto",
+    // new go.Binding("text", "text"),
     // { doubleClick: nodeDoubleClick }, // the Shape will go around the TextBlock
-    new go.Binding("location", "loc", go.Point.parse).makeTwoWay(
-      go.Point.stringify
-    ),
+    // new go.Binding("location", "loc", go.Point.parse).makeTwoWay(
+    //   go.Point.stringify
+    // ),
     $(
       go.Shape,
       "Rectangle",
@@ -137,11 +219,81 @@ function initDiagram() {
       // Shape.fill is bound to Node.data.color
       new go.Binding("fill", "color")
     ),
+
     $(
-      go.TextBlock,
-      { margin: 8, editable: true }, // some room around the text
-      new go.Binding("text").makeTwoWay()
+      go.Panel,
+      "Horizontal",
+      $(
+        go.Picture,
+        { margin: 10, width: 50, height: 50, background: "red" },
+        new go.Binding("source")
+      ),
+
+      $(
+        go.Panel,
+        "Table",
+        {
+          minSize: new go.Size(150, NaN),
+          // maxSize: new go.Size(150, NaN),
+          margin: new go.Margin(6, 10, 0, 6),
+          defaultAlignment: go.Spot.Left,
+        },
+        $(go.RowColumnDefinition, { column: 2, width: 4 }),
+        $(
+          go.TextBlock,
+          "stretch: Horizontal",
+          // textStyle(), // the name
+          {
+            row: 0,
+            column: 0,
+            columnSpan: 5,
+            // stretch: go.GraphObject.Horizontal,
+            font: "12pt Segoe UI,sans-serif",
+            editable: true,
+            isMultiline: false,
+            minSize: new go.Size(10, 16),
+          },
+          new go.Binding("text", "text").makeTwoWay()
+        ),
+        // $(go.TextBlock, "id: ", { row: 1, column: 0 }),
+        $(
+          go.TextBlock,
+          {
+            row: 1,
+            column: 0,
+            // columnSpan: 4,
+            editable: true,
+            isMultiline: false,
+            // minSize: new go.Size(10, 14),
+            // margin: new go.Margin(0, 0, 0, 3),
+          },
+          new go.Binding("text", "key", function (v) {
+            return "ID: " + v;
+          })
+        ),
+        $(
+          go.TextBlock,
+          // textStyle(),
+          { name: "boss", row: 2, column: 0 }, // we include a name so we can access this TextBlock when deleting Nodes/Links
+          new go.Binding("text", "parent", function (v) {
+            return "Boss: " + v;
+          })
+        )
+      )
+
+      // $(
+      //   go.TextBlock,
+      //   "Default Text",
+      //   { margin: 12, stroke: "white", font: "bold 16px sans-serif" },
+      //   new go.Binding("text", "text")
+      // )
     )
+
+    // $(
+    //   go.TextBlock,
+    //   { margin: 8, editable: true }, // some room around the text
+    //   new go.Binding("text").makeTwoWay()
+    // )
   );
 
   return myDiagram;
@@ -152,17 +304,20 @@ function handleModelChange(changes) {
 }
 
 const Diagrama = () => {
+  const { users } = useContext(RedmineContext);
+
+  console.log(users);
   return (
     <ReactDiagram
       initDiagram={initDiagram}
       divClassName={classes["diagram-component"]}
       nodeDataArray={nodeDataArra}
-      linkDataArray={[
-        { key: -1, from: 0, to: 1 },
-        { key: -2, from: 0, to: 2 },
-        { key: -3, from: 0, to: 3 },
-        { key: -4, from: 0, to: 4 },
-      ]}
+      // linkDataArray={[
+      //   { key: -1, from: 0, to: 1 },
+      //   { key: -2, from: 0, to: 2 },
+      //   { key: -3, from: 0, to: 3 },
+      //   { key: -4, from: 0, to: 4 },
+      // ]}
       onModelChange={handleModelChange}
     />
   );
